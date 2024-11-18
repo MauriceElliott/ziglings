cbf0998fff173b9fa7c1170b58b95e43838091bf
MauriceElliott <mauricemelliott@gmail.com>
2024-11-18 20:33:10 +0000

    Continued

--- exercises/058_quiz7.zig
+++ exercises/058_quiz7.zig
@@ -192,8 +192,8 @@ const TripItem = union(enum) {
             // Oops! The hermit forgot how to capture the union values
             // in a switch statement. Please capture both values as
             // 'p' so the print statements work!
-            .place => |a| std.debug.print("{s}", .{a.name}),
-            .path => |b| std.debug.print("--{}->", .{b.dist}),
+            .place => |p| print("{s}", .{p.name}),
+            .path => |p| print("--{}->", .{p.dist}),
         }
     }
 };
@@ -309,7 +309,7 @@ const HermitsNotebook = struct {
     //
     // Looks like the hermit forgot something in the return value of
     // this function. What could that be?
-    fn getTripTo(self: *HermitsNotebook, trip: []?TripItem, dest: *Place) void {
+    fn getTripTo(self: *HermitsNotebook, trip: []?TripItem, dest: *Place) TripError!void {
         // We start at the destination entry.
         const destination_entry = self.getEntry(dest);
 
